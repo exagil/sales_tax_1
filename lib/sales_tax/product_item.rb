30173129fdada5a7cf0a3a6b7cbb7a5e686cc21b
Chirag Aggarwal <me@chi6rag.net>
2015-10-27 12:19:13 +0530

    ProductItem: Adding test to expect #new with exempt tax true creates a tax exempt product item Adding tax exempt in constructor

--- lib/sales_tax/product_item.rb
+++ lib/sales_tax/product_item.rb
@@ -1,10 +1,10 @@
 module SalesTax
   class ProductItem
-    def initialize name, price_per_item, quantity
+    def initialize name, price_per_item, quantity, tax_exempt=false
       @name = name
       @price_per_item = price_per_item
       @quantity = quantity
-      @exempt = false
+      @tax_exempt = tax_exempt
       @imported = false
     end
   end
--- spec/product_item_spec.rb
+++ spec/product_item_spec.rb
@@ -4,14 +4,21 @@ module SalesTax
   RSpec.describe ProductItem do
     describe '#new' do
       context 'with valid params' do
-        it 'creates a new non exempt non imported ProductItem' do
+        it 'creates a new non tax exempt non imported ProductItem' do
           p = ProductItem.new('book', 12.00, 2)
           expect(p.instance_eval "@name").to eq "book"
           expect(p.instance_eval "@price_per_item").to eq 12.00
           expect(p.instance_eval "@quantity").to eq 2
-          expect(p.instance_eval "@exempt").to eq false
+          expect(p.instance_eval "@tax_exempt").to eq false
           expect(p.instance_eval "@imported").to eq false
         end
+
+        it 'creates a new tax exempt non imported ProductItem if explicitly specified' do
+          p = ProductItem.new('book', 12.00, 2, true)
+          expect(p.instance_eval "@tax_exempt").to eq true
+          expect(p.instance_eval "@imported").to eq false
+        end
+
       end
     end
   end
